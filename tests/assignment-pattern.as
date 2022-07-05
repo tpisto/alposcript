877f6edae66ff908e50165134a0e256a42abc897
Tommi Pisto <tommi@pis.to>
2022-07-05 14:59:21 +0300

    Assignment pattern

---
+++ tests/assignment-pattern.as
@@ -0,0 +1,7 @@
+# Assignment pattern
+fn test(a = {}) ->
+  let { test } = a
+
+(b = {}) => {
+  let { c } = b;
+}
---
+++ tests/results/assignment-pattern.js
@@ -0,0 +1,13 @@
+let a, b;
+
+function test(a = {}) {
+  let {
+    test
+  } = a;
+}
+
+(b = {}) => {
+  let {
+    c
+  } = b;
+};
