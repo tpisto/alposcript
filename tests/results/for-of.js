6b07b99dedb1c36b34f7c209bd4699f5d365292e
Tommi Pisto <tommi@pis.to>
2022-12-30 18:33:36 +0200

    Revert "Convert for loop to follow javascript convention"
    This reverts commit 6797340e45c51e595d8f961b995d1c3f164fe347.

---
+++ tests/results/for-of.js
@@ -0,0 +1,19 @@
+for (const [a, b] of c) {
+  let a;
+  a = 1;
+};
+
+for (const a of d) {
+  console.log("jee");
+};
+
+for (let [key, value] of Object.entries(myObject)) {
+  let a;
+  a = 1;
+};
+
+for (let [key2, value2] of Object.entries(this.myObject)) {
+  let a = {
+    b: 1
+  };
+};
